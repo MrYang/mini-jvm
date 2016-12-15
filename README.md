e916b78f54af4f7dffc7d6057c73ab54160c446b
Mr.Yang <paul.wide@gmail.com>
2016-12-15 16:52:07 +0800

    修改Readme文件, 添加Method toString中的局部变量信息

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 ## java class 文件解析
 
-使用java解析class 文件(基于1.7版本), 基本上是仿照apache bcel 包。
+使用java解析class 文件(基于1.7版本), 基本上是参照apache bcel 包。
 
 在apache包的基础上,扩展了一些缺失的常量池类型,及属性类型
+
+class文件格式:
+
+![](http://coolshell.cn//wp-content/uploads/2013/03/1.png)
+
+其中大部分常量及属性拥有各自不同的数据结构,需要每个每个去解析。
+
+从ClassParser类开始, 按照顺序读取class文件,最终得到一个JavaClass 数据结构
+
+```java
+     readID();
+     readVersion();
+     readConstantPool();
+     readClassInfo();
+     readInterfaces();
+     readFields();
+     readMethods();
+     readAttributes();
+```
--- src/main/java/com/zz/parser/Method.java
+++ src/main/java/com/zz/parser/Method.java
@@ -72,7 +72,7 @@ public class Method extends FieldOrMethod {
         String access = Utility.accessToString(access_flags);
         String signature = constant_pool.constantToString(signature_index, Constants.CONSTANT_Utf8);
         String name = constant_pool.constantToString(name_index, Constants.CONSTANT_Utf8);
-        StringBuilder buf = new StringBuilder(Utility.methodSignatureToString(signature, name, access));
+        StringBuilder buf = new StringBuilder(Utility.methodSignatureToString(signature, name, access, true, getLocalVariableTable()));
 
         for(int i=0; i < attributes_count; i++) {
             Attribute a = attributes[i];
